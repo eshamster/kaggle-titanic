ce878e9e3d491a52196e6b8d31e629ee3157f320
eshamster <hamgoostar@gmail.com>
2015-10-22 02:09:13 +0900

    Fix a underlying bug.
    kaggle-titanic.csv::equal-name could judge as true if both arguments are not symbol. So there was possibility to replace string by symbol in kaggle-titanic.csv:re-intern-symbols (such case as '(('test "test")))
    
    	modified:   src/csv.lisp

--- src/csv.lisp
+++ src/csv.lisp
@@ -15,9 +15,9 @@
        (nth it data-line)))
 
 (defun equal-name (l r)
-  (labels ((get-name (x)
-             (if (symbolp x) (symbol-name x) x)))
-    (equal (get-name l) (get-name r))))
+  (and (symbolp l)
+       (symbolp r)
+       (equal (symbol-name l) (symbol-name r))))
 
 (defun re-intern-symbols (body-lst name-lst)
   (let ((replace-lst (mapcar (lambda (name) (cons name name))
