09afb8f16e6a8c5eaf20526f67ae286e4eec7a91
eshamster <hamgoostar@gmail.com>
2015-10-21 21:41:28 +0900

    Fix convert-raw-data-one-line to intern by more safely way.
    	modified:   src/csv.lisp

--- src/csv.lisp
+++ src/csv.lisp
@@ -14,18 +14,42 @@
   (aif (position target head-line :test #'equal)
        (nth it data-line)))
 
+(defun reconstruct-list (lst fn-elem)
+  (labels ((rec (result rest)
+             (if rest
+                 (dolist (elem rest)
+                   (setf result
+                         (cons (if (listp elem)
+                                   (rec nil elem)
+                                   (funcall fn-elem elem))
+                               result))))
+             (reverse result)))
+    (rec nil lst)))
+
+(defun rec-intern-by-args (body-lst arg-lst)
+  (reconstruct-list body-lst
+                    (lambda (elem)
+                      (if (symbolp elem)
+                          (if (find (symbol-name elem) (mapcar #'symbol-name arg-lst)
+                                    :test #'equal)
+                              (intern (symbol-name elem))
+                              elem)
+                          elem))))
+
 (defmacro convert-raw-data-one-line (head-line data-line &body body)
   (once-only (head-line data-line)
     `(list ,@(mapcar
               (lambda (process)
                 (let ((arg-lst (if (listp (car process))
-                                   (mapcar (compose #'intern #'string-upcase) (car process))
-                                   '(it)))
+                                   (mapcar (lambda (str)
+                                             (intern (string-upcase str)))
+                                           (car process))
+                                   '(:it)))
                       (find-lst (if (listp (car process))
                                     (car process)
                                     (list (car process)))))
                   `((lambda (,@arg-lst)
-                      ,@(cdr process))
+                      ,@(rec-intern-by-args (cdr process) arg-lst))
                     ,@(mapcar
                        (lambda (arg) 
                          `(find-target-value ,arg ,head-line ,data-line))
